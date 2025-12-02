8e921930fb646e21e2d761e3606878422bad7a7f
zbinden-sofia <zbinden.sofia@gmail.com>
2025-12-02 17:45:23 +0100

    Day 2 part 2
    Not happy that I loop now through the whole ranges... not sure if it is
    possible to jump to the next possibile id

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -4,41 +4,42 @@ with open('day2/data.txt') as infile:
 lines = line[0].strip().split(',')
 id_ranges = [range(int(l.split('-')[0]),int(l.split('-')[1])+1) for l in lines]
 
+def get_all_dividers(num):
+    dividers = []
+
+    for i in range(2,num+1):
+        if num % i == 0:
+            dividers.append(i)
+
+    return dividers
+
 def get_num_invalid_IDs(id_range):
-    num_invalid = 0
     sum_invalid = 0
     first_id = id_range[0]
     last_id = id_range[-1]
+    invalid_ids = []
 
     id = first_id
     while id <= last_id:
         idstring = str(id)
 
-        if len(idstring)%2 == 0:
+        for divider in get_all_dividers(len(idstring)):
             # get number to check
-            id_half = idstring[0:len(idstring)//2]
-            to_check = int(id_half + id_half)
+            id_part = idstring[0:len(idstring)//divider]
+            to_check = int(id_part * divider)
 
             # check if the number is in the range
             if first_id <= to_check <= last_id:
-                    num_invalid += 1
+                if to_check not in invalid_ids:
                     sum_invalid += to_check
-                
-            # forward to number afterwards
-            id = (int(id_half)+1)*pow(10,len(id_half))
-            continue
-
+                    invalid_ids.append(to_check)
 
         id += 1
         
-    return num_invalid, sum_invalid
+    return sum_invalid
 
-num_invalid_ids = 0
 sum_invalid_ids = 0
 for id_range in id_ranges:
-    num, sum = get_num_invalid_IDs(id_range)
+    sum_invalid_ids += get_num_invalid_IDs(id_range)
 
-    num_invalid_ids += num
-    sum_invalid_ids += sum
-
-print(f"{num_invalid_ids} invalid IDs found, summed up as {sum_invalid_ids}")
+print(f"There are invalid IDs found, summed up to {sum_invalid_ids}")
